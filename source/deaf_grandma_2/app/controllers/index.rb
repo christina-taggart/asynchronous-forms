25172a5e3c2bd3332bdca5468a9cedbd26aa5d80
Spencer Smitherman & David Goodman <pair+bigspencey.dgoodman1224@devbootcamp.com>
2014-02-05 11:50:43 -0800

    Prevent reload and enabled AJAX

--- source/deaf_grandma_2/app/controllers/index.rb
+++ source/deaf_grandma_2/app/controllers/index.rb
@@ -5,5 +5,10 @@ get '/' do
 end
 
 post '/grandma' do
-  "Implement the /grandma route yourself.<br>Params: <code>#{params.inspect}</code>"
+  input = params[:user_input]
+  if input == input.upcase
+    @grandma = "I agree, JFK was quite handsome."
+  else
+    @grandma = "SPEAK UP HOMIE"
+  end
 end
--- source/deaf_grandma_2/public/js/application.js
+++ source/deaf_grandma_2/public/js/application.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#the_form').submit(function(event){
+    event.preventDefault()
+    myRequest = $.ajax({
+      type: "POST",
+      url: '/grandma',
+      data: $(this).serialize() //Creates a jquery object
+    })
+    myRequest.done(function(server_data) {
+      console.log("This worked" + server_data)
+      // select element on the page
+      // change the text of that element to show
+      // the server data
+      $('#someid').text("Grandma says: " + server_data)
+    })
+    myRequest.fail(function(server_data) {
+      alert("this failed")
+    })
+  })
 });
